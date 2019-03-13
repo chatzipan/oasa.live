1006cfdff6a535b84b411492c401749e7bd308cc
Vasilis Chatzipanagiotis <vchatzipan@gmail.com>
2019-03-13 21:31:10 +0100

    test

--- src/lambda/netlify/getStaticData.js
+++ src/lambda/netlify/getStaticData.js
@@ -1,18 +1,26 @@
-// import { fetchFromS3 } from '../helpers/s3.js'
+import { fetchFromS3 } from '../helpers/s3.js'
 
 export async function handler(event, context) {
   try {
-    console.log({
-      AWS_ACCESS_KEYID: process.env.AWS_ACCESS_KEYID,
-      AWS_SECRET_ACCESSKEY: process.env.AWS_SECRET_ACCESSKEY,
-    })
+    const [
+      _lines,
+      _coordinates,
+      _routeDetails,
+      _routeStops,
+    ] = await Promise.all([
+      fetchFromS3('linesList.json'),
+      fetchFromS3('routePaths.json'),
+      fetchFromS3('routeList.json'),
+      fetchFromS3('routeStops.json'),
+    ])
+    const lines = JSON.parse(_lines)
+    const coordinates = JSON.parse(_coordinates)
+    const routeDetails = JSON.parse(_routeDetails)
+    const stops = JSON.parse(_routeStops)
 
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        _KEYID: process.env.AWS_ACCESS_KEYID,
-        AWS_SECRET_ACCESSKEY: process.env.AWS_SECRET_ACCESSKEY,
-      }),
+      body: JSON.stringify({ coordinates, lines, routeDetails, stops }),
     }
   } catch (err) {
     console.log(err)
