5814285aa4745bf0dce8ef4718390eeb25ab2615
Vasilis Chatzipanagiotis <vchatzipan@gmail.com>
2019-03-13 21:46:17 +0100

    test

--- src/lambda/helpers/s3.js
+++ src/lambda/helpers/s3.js
@@ -1,8 +1,4 @@
 const AWS = require('aws-sdk')
-console.log({
-  AWS_ACCESS_KEYID: process.env.AWS_ACCESS_KEYID,
-  AWS_SECRET_ACCESSKEY: process.env.AWS_SECRET_ACCESSKEY,
-})
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEYID,
