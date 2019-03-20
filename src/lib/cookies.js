c5e5ee79814e1c8e35f43e32fe3f013ab797b99e
Vasilis Chatzipanagiotis <vchatzipan@gmail.com>
2019-03-20 19:08:03 +0100

    fix build

--- src/lib/cookies.js
+++ src/lib/cookies.js
@@ -1,3 +1,5 @@
+// const document =
+//   typeof window !== 'undefined' ? window.localStorage : mockStorage
 function setCookie(name, value, days) {
   let expires = ''
   if (days) {
@@ -5,21 +7,30 @@ function setCookie(name, value, days) {
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
     expires = '; expires=' + date.toUTCString()
   }
-  document.cookie = name + '=' + (value || '') + expires + '; path=/'
+
+  if (typeof window !== 'undefined') {
+    document.cookie = name + '=' + (value || '') + expires + '; path=/'
+  }
 }
 
 function getCookie(name) {
   const nameEQ = name + '='
-  const ca = document.cookie.split(';')
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i]
-    while (c.charAt(0) === ' ') c = c.substring(1, c.length)
-    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length)
+  if (typeof window !== 'undefined') {
+    const ca = document.cookie.split(';')
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i]
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length)
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length)
+    }
   }
+
   return null
 }
+
 function deleteCookie(name) {
-  document.cookie = name + '=; Max-Age=-99999999;'
+  if (typeof window !== 'undefined') {
+    document.cookie = name + '=; Max-Age=-99999999;'
+  }
 }
 
 export { deleteCookie, getCookie, setCookie }
