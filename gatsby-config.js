aa5dd17033e8ce353fcfb4a066dbd13f3364beaa
Vasilis Chatzipanagiotis <vchatzipan@gmail.com>
2019-03-24 21:21:11 +0100

    remove proxy middleware

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,3 @@
-const proxy = require('http-proxy-middleware')
-
 module.exports = {
   siteMetadata: {
     title: 'Athens live bus telematics',
@@ -19,17 +17,6 @@ module.exports = {
       'ΟΑΣΑ',
     ],
     author: `https://github.com/chatzipan`,
-  },
-  developMiddleware: app => {
-    app.use(
-      '/.netlify/functions/',
-      proxy({
-        target: 'http://localhost:9000',
-        pathRewrite: {
-          '/.netlify/functions/': '',
-        },
-      })
-    )
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
