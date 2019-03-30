299d2cb8b01eef4dfe59e064a9ec77ee287135ac
Vasilis Chatzipanagiotis <vchatzipan@gmail.com>
2019-03-30 23:29:57 +0100

    remove visibility from fake content

--- src/components/FakeContent/FakeContent.js
+++ src/components/FakeContent/FakeContent.js
@@ -3,7 +3,7 @@ import React from 'react'
 import mock from '../../assets/mocks/linesList'
 
 const FakeContent = () => (
-  <dl>
+  <dl style={{ position: 'fixed', bottom: 0, visibility: 'hidden' }}>
     {Object.values(mock).map(({ line, descr }, i) => (
       <div key={i}>
         <dt key={`${i}_dt`}>{line}</dt>
