f439b1f2e20b0b276e43c8b09bd611f4bcd76aca
Vasilis Chatzipanagiotis <vchatzipan@gmail.com>
2019-11-03 11:15:55 +0100

    create node server for stops

---
+++ src/lambda/server.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const port = 6349
+const url = require('url')
+
+const { fetch } = require('./helpers/fetch.js')
+const { GET_STOP_ARRIVALS } = require('./helpers/api.js')
+
+const requestHandler = async (request, response) => {
+  console.time('AWS: fetch stop arrivals time')
+  try {
+    /* eslint-disable */
+    const url_parts = url.parse(request.url, true)
+    const { stopCode } = url_parts.query
+    const arrivals = stopCode
+      ? await fetch(`${GET_STOP_ARRIVALS}${stopCode}`)
+      : null
+
+    console.timeEnd('AWS: fetch stop arrivals time')
+    response.end(JSON.stringify(arrivals))
+  } catch (err) {
+    console.log(err)
+    res.statusCode = 500
+    res.write({ msg: err.message }) //write a response to the client
+    res.end()
+  }
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, err => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
