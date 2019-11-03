9e2ff3ab91fd99f5d4679c7d20d349f2453844f9
Vasilis Chatzipanagiotis <vchatzipan@gmail.com>
2019-11-03 10:21:34 +0100

    rebuild

---
+++ src/lambda/netlify/fetchStopArrivals.js
@@ -0,0 +1,29 @@
+const { fetch } = require('../helpers/fetch.js')
+const { GET_STOP_ARRIVALS } = require('../helpers/api.js')
+
+exports.handler = async (event, context) => {
+  console.time('AWS: fetch stop arrivals time')
+  try {
+    const {
+      queryStringParameters: { stopCode },
+    } = event
+    const arrivals = await fetch(`${GET_STOP_ARRIVALS}${stopCode}`)
+    console.timeEnd('AWS: fetch stop arrivals time')
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(arrivals),
+    }
+  } catch (err) {
+    console.log(err)
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ msg: err.message }),
+    }
+  }
+}
